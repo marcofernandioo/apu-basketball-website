3bc81c3d559fe800580d81b59415c3b82818de39
marco <marcofernandioo@gmail.com>
2022-06-08 23:03:05 +0800

    Added many images and files, and added Navbar in each page.

---
+++ js/FAQ.js
@@ -0,0 +1,15 @@
+const accordionBtns = document.querySelectorAll(".item-header");
+
+accordionBtns.forEach((accordion) => {
+  accordion.onclick = function () {
+    this.classList.toggle("active");
+
+    let content = this.nextElementSibling;
+
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  };
+});
